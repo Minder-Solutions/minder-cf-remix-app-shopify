3ab780223244243c2d402890ecc99088546633ea
Daniel Eisen <eisen.dan@gmail.com>
2025-08-23 16:53:09 -0700

    some changes

---
+++ app/root.tsx
@@ -0,0 +1,54 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useNavigate,
+} from "@remix-run/react";
+import { useEffect } from "react";
+
+// https://shopify.dev/docs/api/app-home/using-polaris-components
+export default function App() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    //@ts-ignore
+    const handleNavigate = (event) => {
+      const target = event.detail?.target || event.target;
+      const href = target.getAttribute('href');
+      if (href) navigate(href);
+    };
+
+    document.addEventListener('shopify:navigate', handleNavigate);
+    return () => {
+      document.removeEventListener('shopify:navigate', handleNavigate);
+    };
+  }, [navigate]);
+
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
+        <link
+          rel="stylesheet"
+          href="/the-polaris.css"
+        /> 
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <script src="https://cdn.shopify.com/shopifycloud/app-bridge-ui-experimental.js"></script>
+      </body>
+    </html>
+  );
+}
